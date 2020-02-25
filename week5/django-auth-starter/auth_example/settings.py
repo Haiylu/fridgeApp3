90fd9372931b8aca6fc373a5c39593b035bb4fad
Haiylu <Haiylu.Kibret@gmail.com>
2020-02-25 18:39:57 -0500

    fridgeAppin the process

--- week5/django-auth-starter/auth_example/settings.py
+++ week5/django-auth-starter/auth_example/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
+    'user_auth.apps.UserAuthConfig',
     'user_auth.apps.UserAuthConfig'
 ]
 
